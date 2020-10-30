eafb6a46c9f0358f52371a13b064b939ecd33eb1
fxbrbe <francois-xavier.barbier@edu.devinci.fr>
2020-10-30 12:56:13 +0100

    first commit

---
+++ src/components/navigation/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-blue-300 p-2">
+    <a href="#" @click="changeComponent('cours-view')">Tous mes cours</a> | 
+    <a href="#" @click="changeComponent('selection-list')">Mes cours préférés</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "nav-bar",
+  methods: {
+    changeComponent(value) {
+      this.$emit('change-component', value)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
